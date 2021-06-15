c8e1b3b09c17a0525202a6f48f8af13acfc2e5a4
Janelle Levesque <85463875+nellevvy@users.noreply.github.com>
2021-06-15 00:17:34 +0000

    1.1a and 1.2 a

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,9 +3,9 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = input.question("What is your name? ");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
+let question = input.question("How old are you? ");
 let correctAnswer;
 let candidateAnswer;
 let questions;
@@ -13,15 +13,14 @@ let correctAnswers;
 let candidateAnswers;
 
 
-function askForName() {
+function askForName(candidateName) {
   // TODO 1.1b: Ask for candidate's name //
-
+  console.log(candidateName);
 }
 
-function askQuestion() {
+function askQuestion(candidateAnswer) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  console.log(question);
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -30,7 +29,7 @@ function gradeQuiz(candidateAnswers) {
 
 
   let grade;
-  
+
 
   return grade;
 }
@@ -38,7 +37,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
