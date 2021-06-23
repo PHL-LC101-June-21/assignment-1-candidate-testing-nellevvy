4bcdcf321e8bbdc9b056b291610c8c77b13684c5
Janelle Levesque <85463875+nellevvy@users.noreply.github.com>
2021-06-23 00:14:48 +0000

    Questions and output working

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,38 +5,38 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = input.question("What is your name? ");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = input.question("Who was the first American woman in space? ");
+//let question = input.question("Who was the first American woman in space? ");
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let candidateAnswers = [];
 
-let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?"];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 
-let correctAnswers = ["Sally Ride", "true", "40", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"]
+let correctAnswers = ["Sally Ride", "true", "40","Trajectory", "3"]
 
  // TODO 1.1b: Ask for candidate's name //
 function askForName(candidateName) {
-  console.log(candidateName);
+  console.log(`Hello ${candidateName}.`);
 }
 
 for (let i = 0; i < questions.length; i++){
   candidateAnswers.push(input.question(questions[i]));
 }
 
-console.log(candidateAnswers);
+for (let i = 0; i < candidateAnswers.length; i++){
+  console.log(`You answered ${candidateAnswers[i]} and the correct answer is ${correctAnswers[i]}`);
+}
 
  // TODO 1.1c: Ask for candidate's name //
 function runProgram() {
   askForName(candidateName);
   askQuestions();
- // gradeQuiz(this.candidateAnswers);
+  //gradeQuiz(this.candidateAnswers);
 }
 
 function askQuestions(candidateAnswers) {
   
 }
-
-
 
 
 
@@ -74,7 +74,7 @@ function gradeQuiz(candidateAnswers) {
 // And don't change these or your program will not run as expected //
 module.exports = {
   candidateName: candidateName,
-  question: question,
+  //question: question,
   correctAnswer: correctAnswer,
  // candidateAnswer: candidateAnswer,
   questions: questions,
